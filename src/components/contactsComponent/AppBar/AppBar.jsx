e35e4a3c22ae202e3f21c56bfffed5dca2af425f
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-07-19 15:19:00 +0300

    refacted appearing header-app

--- src/components/contactsComponent/AppBar/AppBar.jsx
+++ src/components/contactsComponent/AppBar/AppBar.jsx
@@ -1,18 +1,35 @@
-import { StatusFilter } from 'components/contactsComponent/StatusFilter/StatusFilter';
-import { ContactCounter } from 'components/contactsComponent/ContactCounter/ContactCounter';
+import { Navigation } from 'components/Navigation/Navigation';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { AuthNav } from 'components/authComponent/AuthNav/AuthNav';
+import { useAuth } from 'hooks';
 import css from './AppBar.module.css';
 
-export function AppBar() {
+export const AppBar = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
-    <header className={css.wrapper}>
-      <section className={css.section}>
-        <h2 className={css.title}>Contacts</h2>
-        <ContactCounter />
-      </section>
-      <section className={css.section}>
-        <h2 className={css.title}>Filter contacts by status</h2>
-        <StatusFilter />
-      </section>
+    <header className={css.header}>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
-}
+};
+
+// import { StatusFilter } from 'components/contactsComponent/StatusFilter/StatusFilter';
+// import { ContactCounter } from 'components/contactsComponent/ContactCounter/ContactCounter';
+// import css from './AppBar.module.css';
+
+// export function AppBar() {
+//   return (
+//     <header className={css.wrapper}>
+//       <section className={css.section}>
+//         <h2 className={css.title}>Contacts</h2>
+//         <ContactCounter />
+//       </section>
+//       <section className={css.section}>
+//         <h2 className={css.title}>Filter contacts by status</h2>
+//         <StatusFilter />
+//       </section>
+//     </header>
+//   );
+// }

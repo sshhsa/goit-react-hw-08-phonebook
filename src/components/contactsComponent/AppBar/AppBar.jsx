d3615e45b66e74c7f92b1aecc76e82c0e465b0a9
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-07-22 10:32:43 +0300

    added any changes and deleted unappropriate code

--- src/components/contactsComponent/AppBar/AppBar.jsx
+++ src/components/contactsComponent/AppBar/AppBar.jsx
@@ -14,22 +14,3 @@ export const AppBar = () => {
     </header>
   );
 };
-
-// import { StatusFilter } from 'components/contactsComponent/StatusFilter/StatusFilter';
-// import { ContactCounter } from 'components/contactsComponent/ContactCounter/ContactCounter';
-// import css from './AppBar.module.css';
-
-// export function AppBar() {
-//   return (
-//     <header className={css.wrapper}>
-//       <section className={css.section}>
-//         <h2 className={css.title}>Contacts</h2>
-//         <ContactCounter />
-//       </section>
-//       <section className={css.section}>
-//         <h2 className={css.title}>Filter contacts by status</h2>
-//         <StatusFilter />
-//       </section>
-//     </header>
-//   );
-// }
